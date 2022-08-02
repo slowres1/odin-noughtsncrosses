3091796e1af6d7c8b613a7ae4396f9a47b383d9a
Stanley Lowres <slowres@gmail.com>
2022-08-02 16:50:39 +0100

    made progress, bit stuck. Come back later

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const container = document.querySelector('.container');
-const board = document.querySelector('.game-board');
 
+//game board module function.
 const gameBoard = (function() {
+    //private array
     const contents = ['','A','','','X','','','',''];
 
-    function renderBoard(board) {
+    //private variable for the board
+    const board = document.querySelector('.game-board');
+    //displays the board using the current contents
+    //removes all children then re-populates
+    function renderBoard() {
+        while (board.firstChild) {
+            board.removeChild(board.lastChild);
+        }
         for (let i=0; i<contents.length; i++) {
-            const square = document.createElement('div');
-            square.classList.add('flex');
-            square.textContent = contents[i];
-            board.appendChild(square);
+            createSquare(i);
         }
     }
 
     function changeContent(location, value) {
         contents[location] = value;
     }
-    
+
+    function createSquare(num) {
+        const square = document.createElement('div');
+        square.classList.add('flex');
+        square.setAttribute('id', num);
+        square.textContent = contents[num];
+        board.appendChild(square);
+
+        square.addEventListener('click', () => {
+            changeContent(parseInt(square.id), 'X'),
+            renderBoard();
+        }); 
+    }
+
     return {
         //contents: contents,
         renderBoard: renderBoard,
@@ -24,10 +42,23 @@ const gameBoard = (function() {
     }
 })();
 
-gameBoard.renderBoard(board);
+gameBoard.renderBoard();
 
 
+/*
+//testing area for randomly changing squares and re-rendering.
+//It works!
+const changeButton = document.querySelector('.btn');
+const randomChange = () => {
+    let loc = Math.floor(Math.random() * 9);
+    let num = Math.floor(Math.random() * 10);
+    console.log({loc, num})
+    gameBoard.changeContent(loc, num);
+    gameBoard.renderBoard(board);
+}
 
+changeButton.addEventListener('click', randomChange);
+*/
 /*
 const Player = (name) => {
     const getName = () => name;
